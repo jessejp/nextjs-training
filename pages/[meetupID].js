ef2a7abd61534dd9dcb76cf21d37465430815ec9
Jesse Pernilä <jessepernila@gmail.com>
2022-03-22 10:43:18 +0200

    added getStaticProps function

--- pages/[meetupID].js
+++ pages/[meetupID].js
@@ -1,14 +1,52 @@
 import MeetupDetail from "../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      title="First meetup"
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Hohenzollernbr%C3%BCcke_K%C3%B6ln.jpg/1280px-Hohenzollernbr%C3%BCcke_K%C3%B6ln.jpg"
-      address="Some street 3, Test City"
-      description="Meet up at lunch"
+      title={props.meetupData.title}
+      image={props.meetupData.image}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+}
+
+export function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupID: "m1",
+        },
+      },
+      {
+        params: {
+          meetupID: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export function getStaticProps(context) {
+  // fetch data for a single  meetup
+
+  const meetupID = context.params.meetupID;
+  console.log(meetupID);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupID,
+        title: "First meetup",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Hohenzollernbr%C3%BCcke_K%C3%B6ln.jpg/1280px-Hohenzollernbr%C3%BCcke_K%C3%B6ln.jpg",
+        address: "Some street 3, Test City",
+        description: "Meet up at lunch",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
